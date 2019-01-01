f366e541d12ca11724e641a7f3529caf187ddb44
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-01-01 11:29:05 +0530

    lightning:unsaved

--- force-app/main/default/aura/CaseTimerCalculation/CaseTimerCalculationController.js
+++ force-app/main/default/aura/CaseTimerCalculation/CaseTimerCalculationController.js
@@ -1,40 +1,19 @@
 ({
     initMethod: function (cmp, evt, h) {
-
-        var h = 0, m = 0, s;
-        h = localStorage.getItem("hour") != null ? localStorage.getItem("hour") : 0;
-        m = localStorage.getItem("min") != null ? localStorage.getItem("min") : 0;
-        s = localStorage.getItem("sec") != null ? localStorage.getItem("sec") : 0;
-
-        var interval = window.setInterval(
-            $A.getCallback(function () {
-
-                var value = s++;
-                if (s > 59) {
-                    s = 1;
-                    m++;
-                    if (m > 59) {
-                        m = 1;
-                        h++;
-                    }
-                }
-                console.log(h + ':' + m + ':' + value + ' from component with id ' + cmp.getGlobalId());
-                // code to execute periodically goes here
-                cmp.set("v.hour", h);
-                cmp.set("v.min", m);
-                cmp.set("v.sec", s);
-
-
-            }), 1000
-        );
-
-
-        cmp.set("v.setIntervalId", interval);
+        h.timer(cmp, evt, h);
 
     },
-    playPause: function(cmp,evt,h) {
+    playPause: function (cmp, evt, h) {
+        if (!cmp.get('v.play_pause')) {
+            console.log('sfsd');
+            window.clearInterval(cmp.get("v.setIntervalId"));
+
+        }
+        else {
+            h.timer(cmp, evt, h);
+        }
         cmp.set('v.play_pause', cmp.get('v.play_pause') ? false : true);
-        
+
     },
     handleDestroy: function (cmp, evt, h) {
         localStorage.setItem("hour", cmp.get("v.hour"));
--- force-app/main/default/aura/CaseTimerCalculation/CaseTimerCalculationHelper.js
+++ force-app/main/default/aura/CaseTimerCalculation/CaseTimerCalculationHelper.js
@@ -1,5 +1,30 @@
 ({
-    helperMethod : function(cmp,evt,help) {
-        
+    timer: function (cmp, evt, h) {
+
+        var h = 0, m = 0, s = 0;
+        var interval = window.setInterval(
+            $A.getCallback(function () {
+
+                var value = s++;
+                if (s > 59) {
+                    s = 1;
+                    m++;
+                    if (m > 59) {
+                        m = 1;
+                        h++;
+                    }
+                }
+                console.log(h + ':' + m + ':' + value + ' from component with id ' + cmp.getGlobalId());
+                // code to execute periodically goes here
+                cmp.set("v.hour", h);
+                cmp.set("v.min", m);
+                cmp.set("v.sec", s);
+
+
+            }), 1000
+        );
+
+
+        cmp.set("v.setIntervalId", interval);
     }
 })
