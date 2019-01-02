8e8f802c15d565ddf9714301fd5e7d8f3c9dea0b
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-01-02 16:16:52 +0530

    changes into spring 19

--- force-app/main/default/aura/CaseTimerCalculation/CaseTimerCalculationController.js
+++ force-app/main/default/aura/CaseTimerCalculation/CaseTimerCalculationController.js
@@ -1,29 +1,30 @@
 ({
-    initMethod: function (cmp, evt, h) {
-        h.timer(cmp, evt, h);
-
+    initMethod: function(cmp, evt, help) {
+        setTimeout(function() {
+            var unsaved = cmp.find("unsaved");
+            unsaved.setUnsavedChanges(true, {
+                label: 'Your Session are unsaved'
+            });
+        }, 1000);
+        help.timer(cmp, evt, help);
+        help.loadOwnerId(cmp, evt, help);
+        help.loadSessionData(cmp, evt, help);
+        help.totalTime(cmp, evt, help);
     },
-    playPause: function (cmp, evt, h) {
+    playPause: function(cmp, evt, help) {
         if (!cmp.get('v.play_pause')) {
-            console.log('sfsd');
+            cmp.set('v.isLoading', true);
             window.clearInterval(cmp.get("v.setIntervalId"));
-
-        }
-        else {
-            h.timer(cmp, evt, h);
+            help.createSession(cmp, evt, help);
+        } else {
+            help.timer(cmp, evt, help);
         }
         cmp.set('v.play_pause', cmp.get('v.play_pause') ? false : true);
-
     },
-    handleDestroy: function (cmp, evt, h) {
-        localStorage.setItem("hour", cmp.get("v.hour"));
-        localStorage.setItem("min", cmp.get("v.min"));
-        localStorage.setItem("sec", cmp.get("v.sec"));
-        console.log('clear interval due to navigation');
-        window.clearInterval(cmp.get("v.setIntervalId"));
+    handleSave: function(cmp, evt, help) {
+        help.createSession(cmp, evt, help);
     },
-
-    onTabClosed: function (cmp, evt, help) {
-        alert('hey closed the tab');
+    handleDiscard: function(cmp, evt, help) {
+        help.createSession(cmp, evt, help);
     }
 })
