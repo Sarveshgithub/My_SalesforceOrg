1d5f0db35a4a2bf9ce6c774be6c5192d931c5840
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-01-08 18:34:20 +0530

    reverse back waiting for spring 19

--- force-app/main/default/aura/CaseTimerCalculation/CaseTimerCalculationController.js
+++ force-app/main/default/aura/CaseTimerCalculation/CaseTimerCalculationController.js
@@ -1,80 +1,49 @@
 ({
-    initMethod: function(cmp, evt, help) {
-        if(cmp.get('v.autostart')){
-        	cmp.set('v.play_pause',false);
-            help.timer(cmp, evt, help);
-        }
-        else{
-        	cmp.set('v.play_pause',true);
-        }
-        
-        help.loadOwnerId(cmp, evt, help);
-        help.loadSessionData(cmp, evt, help);
-        help.totalTime(cmp, evt, help);
-    },
-    customSession: function(cmp, evt, help) {
-        let t_id = cmp.find('c_time'),
-            d_id = cmp.find('c_date'),
-            c_timer = t_id.get('v.value'),
-            c_date = d_id.get('v.value'),
-            spt, t = 0,
-            d = false;
-        if (c_timer) {
-            spt = c_timer.split(':');
-            if (spt.length == 3) {
-                for (let i = 0; i < spt.length; i++) {
-                    if (!isNum(spt[i])) {
-                        error(t_id, 'Please enter into correct format');
-                        break;
-                        return;
-                    } else {
-                        t++;
-                        error(t_id, '')
+    initMethod: function (cmp, evt, h) {
+
+        var h = 0, m = 0, s;
+        h = localStorage.getItem("hour") != null ? localStorage.getItem("hour") : 0;
+        m = localStorage.getItem("min") != null ? localStorage.getItem("min") : 0;
+        s = localStorage.getItem("sec") != null ? localStorage.getItem("sec") : 0;
+
+        var interval = window.setInterval(
+            $A.getCallback(function () {
+
+                var value = s++;
+                if (s > 59) {
+                    s = 1;
+                    m++;
+                    if (m > 59) {
+                        m = 1;
+                        h++;
                     }
                 }
-            }
-        } else {
-            error(t_id, 'Please enter into correct format')
-        }
-        if (c_date) {
-            error(d_id, '')
-            d = true
-        } else {
-            error(d_id, 'Please choose date')
-        }
+                console.log(h + ':' + m + ':' + value + ' from component with id ' + cmp.getGlobalId());
+                // code to execute periodically goes here
+                cmp.set("v.hour", h);
+                cmp.set("v.min", m);
+                cmp.set("v.sec", s);
 
-        function isNum(strNum) {
-            return strNum.length == 2 ? !isNaN(parseInt(strNum[0])) ? !isNaN(parseInt(strNum[1])) ? true : false : false : false;
-        }
 
-        function error(auraid, msg) {
-            auraid.setCustomValidity(msg);
-            auraid.reportValidity();
-        }
-        if (t == 3 && d) {
-            cmp.set('v.isLoading', true);
-            help.createCustomSession(cmp, evt, help, c_timer, c_date);
-            cmp.set('v.isShow', cmp.get('v.isShow') ? false : true);
-        }
+            }), 1000
+        );
+
+
+        cmp.set("v.setIntervalId", interval);
+
     },
-    showModal: function(cmp, evt, help) {
-        cmp.set('v.isShow', cmp.get('v.isShow') ? false : true);
-    },
-    playPause: function(cmp, evt, help) {
-        
-        if (!cmp.get('v.play_pause')) {
-            cmp.set('v.isLoading', true);
-            window.clearInterval(cmp.get("v.setIntervalId"));
-            help.createSession(cmp, evt, help);
-        } else {
-            help.timer(cmp, evt, help);
-        }
+    playPause: function(cmp,evt,h) {
         cmp.set('v.play_pause', cmp.get('v.play_pause') ? false : true);
     },
-    handleSave: function(cmp, evt, help) {
-        help.createSession(cmp, evt, help);
+    handleDestroy: function (cmp, evt, h) {
+        localStorage.setItem("hour", cmp.get("v.hour"));
+        localStorage.setItem("min", cmp.get("v.min"));
+        localStorage.setItem("sec", cmp.get("v.sec"));
+        console.log('clear interval due to navigation');
+        window.clearInterval(cmp.get("v.setIntervalId"));
     },
-    handleDiscard: function(cmp, evt, help) {
-        help.createSession(cmp, evt, help);
+
+    onTabClosed: function (cmp, evt, help) {
+        alert('hey closed the tab');
     }
 })
