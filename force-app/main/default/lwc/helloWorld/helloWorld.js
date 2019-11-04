39480243760bb23c1945cea816c2677e80e6fcc7
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-11-04 23:39:35 +0530

    Add update class

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -4,4 +4,12 @@ export default class HelloWorld extends LightningElement {
     changeHandler(event) {
         this.greeting = event.target.value;
     }
+    handleClick(event){
+        console.log('test')
+            const container = this.template.querySelector('.container');
+            console.dir(container)
+            container.innerHTML = '<lightning:input type="date" name="input1" label="Enter a date" />';
+            // ... Do some logic with the container ...
+          
+    }
 }
